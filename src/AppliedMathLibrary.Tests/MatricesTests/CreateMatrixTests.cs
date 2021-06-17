322f47464539e3ae84c694955d0452dc0255fe5c
RostyslavBaitsar <rostik1804@gmail.com>
2021-06-17 20:34:04 +0300

    Add Matrix with various constructors and unit tests

---
+++ src/AppliedMathLibrary.Tests/MatricesTests/CreateMatrixTests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Linq;
+using AppliedMathLibrary.Matrices;
+using FluentAssertions;
+using Xunit;
+
+namespace AppliedMathLibrary.Tests.MatricesTests
+{
+    public class CreateMatrixTests
+    {
+        #region Positive scenarios
+
+        [Theory]
+        [InlineData(1, 1, new double[] { 1 })]
+        [InlineData(1, 2, new double[] { 1, 2 })]
+        [InlineData(1, 3, new double[] { 1, 2, 3 })]
+        [InlineData(2, 1, new double[] { 1, 2 })]
+        [InlineData(3, 1, new double[] { 1, 2, 3 })]
+        [InlineData(2, 2, new double[] { 1, 2, 3, 4 })]
+        [InlineData(2, 3, new double[] { 1, 2, 3, 4, 5, 6 })]
+        [InlineData(3, 2, new double[] { 1, 2, 3, 4, 5, 6 })]
+        public void CreateMatrix_ProperDimensionAndValues_MatrixCreated(int n, int m, double[] values)
+        {
+            var matrixnxm = new Matrix(n, m, values);
+
+            matrixnxm.Should().NotBeNull();
+            matrixnxm.Rows.Should().Be(n);
+            matrixnxm.Columns.Should().Be(m);
+
+            for (var i = 0; i < matrixnxm.Rows; i++)
+            {
+                for (var j = 0; j < matrixnxm.Columns; j++)
+                {
+                    matrixnxm[i, j].Should().Be(values[i * m + j]);
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData(1, new double[] { 1 })]
+        [InlineData(2, new double[] { 1, 2, 3, 4 })]
+        [InlineData(3, new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+        public void CreateSquareMatrix_ProperDimensionAndValues_MatrixCreated(int n, double[] values)
+        {
+            var matrixnxn = new Matrix(n, values);
+
+            matrixnxn.Should().NotBeNull();
+            matrixnxn.Rows.Should().Be(n);
+            matrixnxn.Columns.Should().Be(n);
+
+            for (var i = 0; i < matrixnxn.Rows; i++)
+            {
+                for (var j = 0; j < matrixnxn.Columns; j++)
+                {
+                    matrixnxn[i, j].Should().Be(values[i * n + j]);
+                }
+            }
+        }
+
+        [Fact]
+        public void CreateMatrixWithCopyConstructor_MatrixCreated()
+        {
+            var matrix2x3 = new Matrix(2, 3, 1, 2, 3, 4, 5, 6);
+
+            var copiedMatrix = new Matrix(matrix2x3);
+
+            copiedMatrix.Should().NotBeNull();
+            copiedMatrix.Rows.Should().Be(2);
+            copiedMatrix.Columns.Should().Be(3);
+            copiedMatrix[0, 0].Should().Be(1);
+            copiedMatrix[0, 1].Should().Be(2);
+            copiedMatrix[0, 2].Should().Be(3);
+            copiedMatrix[1, 0].Should().Be(4);
+            copiedMatrix[1, 1].Should().Be(5);
+            copiedMatrix[1, 2].Should().Be(6);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void CreateSquareEmptyMatrix_MatrixCreated(int n)
+        {
+            var matrixnxn = new Matrix(n);
+
+            matrixnxn.Should().NotBeNull();
+            matrixnxn.Rows.Should().Be(n);
+            matrixnxn.Columns.Should().Be(n);
+            matrixnxn.All(x => x == 0).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        [InlineData(1, 3)]
+        [InlineData(2, 1)]
+        [InlineData(3, 1)]
+        [InlineData(2, 2)]
+        public void CreateEmptyMatrix_MatrixCreated(int n, int m)
+        {
+            var matrixnxn = new Matrix(n, m);
+
+            matrixnxn.Should().NotBeNull();
+            matrixnxn.Rows.Should().Be(n);
+            matrixnxn.Columns.Should().Be(m);
+            matrixnxn.All(x => x == 0).Should().BeTrue();
+        }
+
+        #endregion
+
+        #region Negative scenarios
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void CreateSquareEmptyMatrix_ExceptionThrown(int n)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var matrix = new Matrix(n);
+            });
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 0)]
+        [InlineData(0, 1)]
+        [InlineData(-1, 1)]
+        [InlineData(1, -1)]
+        [InlineData(-1, -1)]
+        public void CreateEmptyMatrix_ExceptionThrown(int n, int m)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var matrix = new Matrix(n, m);
+            });
+        }
+
+        [Theory]
+        [InlineData(1, new double[] { 1, 2 })]
+        [InlineData(2, new double[] { 1, 2, 3 })]
+        [InlineData(3, new double[] { })]
+        [InlineData(-1, new double[] { 1 })]
+        public void CreateSquareMatrixWithWrongParameters_ExceptionThrown(int n, double[] values)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var matrix = new Matrix(n, values);
+            });
+        }
+
+        [Theory]
+        [InlineData(1, 2, new double[] { 1 })]
+        [InlineData(2, 1, new double[] { 1, 2, 3 })]
+        [InlineData(3, 3, new double[] { })]
+        [InlineData(-1, 2, new double[] { 1, 2 })]
+        public void CreateMatrixWithWrongParameters_ExceptionThrown(int n, int m, double[] values)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var matrix = new Matrix(n, m, values);
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ src/AppliedMathLibrary/Matrices/Matrix.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppliedMathLibrary.Matrices
+{
+    /// <summary> Matrix nxm with double elements. </summary>
+    public class Matrix : IEnumerable<double>
+    {
+        private readonly int _n;
+        private readonly int _m;
+        private double[,] _elements;
+
+        #region Constructors
+
+        /// <summary> Create nxm matrix whose elements are zeros. </summary>
+        /// <param name="n">Matrix rows number</param>
+        /// <param name="m">Matrix columns number</param>
+        public Matrix(int n, int m)
+        {
+            if (n < 1 || m < 1)
+                throw new ArgumentException($"Can not create {n}x{m} matrix. Such a matrix has no meaning");
+
+            _n = n;
+            _m = m;
+            _elements = new double[_n, _m];
+        }
+
+        /// <summary> Create nxm matrix with provided values. Expect n * m values. </summary>
+        /// <param name="n">Matrix rows number</param>
+        /// <param name="m">Matrix columns number</param>
+        /// <param name="values">Matrix values</param>
+        public Matrix(int n, int m, params double[] values) : this(n, m)
+        {
+            if (_n * _m != values.Length)
+                throw new ArgumentException($"Expected {_n * _m} values but received {values.Length}");
+
+            for (var i = 0; i < _n; i++)
+            {
+                for (var j = 0; j < _m; j++)
+                {
+                    _elements[i, j] = values[i * _m + j];
+                }
+            }
+        }
+
+        /// <summary> Create nxn matrix whose elements are zeros. </summary>
+        /// <param name="n">Matrix rows and columns number</param>
+        public Matrix(int n) : this(n, n) { }
+
+        /// <summary> Create nxn matrix with provided values. Expect n * n values. </summary>
+        /// <param name="n">Matrix rows and columns number</param>
+        /// <param name="values">Matrix values</param>
+        public Matrix(int n, params double[] values) : this(n, n, values) { }
+
+        /// <summary> Create new matrix based on provided. </summary>
+        /// <param name="matrix">Old matrix</param>
+        public Matrix(Matrix matrix)
+        {
+            _n = matrix._n;
+            _m = matrix._m;
+            _elements = matrix._elements.Clone() as double[,];
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Rows => _n;
+        public int Columns => _m;
+        public double this[int i, int j]
+        {
+            get => _elements[i, j];
+            set => _elements[i, j] = value;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public IEnumerator<double> GetEnumerator()
+        {
+            return _elements.Cast<double>().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+    }
+}
