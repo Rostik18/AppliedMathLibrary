51cf84eb080f40100d221d05c0e9395bb8cabe29
RostyslavBaitsar <rostik1804@gmail.com>
2022-12-05 23:37:34 +0200

    Add more matrix benchmarks

--- src/AppliedMathLibrary.Benchmarks/Benchmarks/MatrixBenchmarks.cs
+++ src/AppliedMathLibrary.Benchmarks/Benchmarks/MatrixBenchmarks.cs
@@ -49,16 +49,24 @@ namespace AppliedMathLibrary.Benchmarks.Benchmarks
         }
 
         [Benchmark] public Result<Matrix> CalculateInverse_matrix3x3() => Matrix.CalculateInverse(matrix3x3);
+        [Benchmark] public Result<double> CalculateDeterminant_matrix3x3() => Matrix.CalculateDeterminant(matrix3x3);
         [Benchmark] public Result<Matrix> CalculateInverse_matrix4x4() => Matrix.CalculateInverse(matrix4x4);
+        [Benchmark] public Result<double> CalculateDeterminant_matrix4x4() => Matrix.CalculateDeterminant(matrix4x4);
         [Benchmark] public Result<Matrix> CalculateInverse_matrix5x5() => Matrix.CalculateInverse(matrix5x5);
+        [Benchmark] public Result<double> CalculateDeterminant_matrix5x5() => Matrix.CalculateDeterminant(matrix5x5);
         [Benchmark] public Result<Matrix> CalculateInverse_matrix10x10() => Matrix.CalculateInverse(matrix10x10);
+        [Benchmark] public Result<double> CalculateDeterminant_matrix10x10() => Matrix.CalculateDeterminant(matrix10x10);
 
-        //|                       Method |             Mean |          Error |         StdDev |    Allocated |
-        //|----------------------------- |-----------------:|---------------:|---------------:|-------------:|
-        //|   CalculateInverse_matrix3x3 |         1.328 us |      0.0094 us |      0.0083 us |         2 KB |
-        //|   CalculateInverse_matrix4x4 |         7.822 us |      0.0689 us |      0.0538 us |         9 KB |
-        //|   CalculateInverse_matrix5x5 |        49.613 us |      0.9385 us |      1.0431 us |        60 KB |
-        //| CalculateInverse_matrix10x10 | 2,849,996.260 us | 56,265.7571 us | 64,795.7295 us | 3,399,511 KB |
+        //|                           Method |               Mean |             Error |            StdDev |       Allocated |
+        //|--------------------------------- |-------------------:|------------------:|------------------:|----------------:|
+        //|       CalculateInverse_matrix3x3 |         2,085.0 ns |          87.35 ns |         257.54 ns |         1,680 B |
+        //|   CalculateDeterminant_matrix3x3 |           359.5 ns |          11.11 ns |          31.71 ns |           312 B |
+        //|       CalculateInverse_matrix4x4 |        11,376.9 ns |         634.73 ns |       1,871.50 ns |         9,720 B |
+        //|   CalculateDeterminant_matrix4x4 |         2,305.8 ns |          71.84 ns |         210.69 ns |         1,824 B |
+        //|       CalculateInverse_matrix5x5 |        77,476.9 ns |       2,741.24 ns |       8,082.61 ns |        61,536 B |
+        //|   CalculateDeterminant_matrix5x5 |        12,664.5 ns |         565.19 ns |       1,648.69 ns |        10,120 B |
+        //|     CalculateInverse_matrix10x10 | 4,422,296,719.0 ns | 142,455,953.25 ns | 420,034,592.30 ns | 3,481,088,904 B |
+        //| CalculateDeterminant_matrix10x10 |   264,502,964.9 ns |   5,256,612.65 ns |  10,252,619.27 ns |   316,462,512 B |
 
         // 1 us      : 1 Microsecond (0.000001 sec)
     }
--- src/AppliedMathLibrary.Benchmarks/Program.cs
+++ src/AppliedMathLibrary.Benchmarks/Program.cs
@@ -1,3 +1,5 @@
-﻿using BenchmarkDotNet.Running;
+﻿using AppliedMathLibrary.Benchmarks.Benchmarks;
+using BenchmarkDotNet.Running;
 
-var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+//var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+var summary = BenchmarkRunner.Run<MatrixBenchmarks>();
